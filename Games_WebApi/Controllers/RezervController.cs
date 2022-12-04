d83804f359bc990fb5f3500372505343bf89644a
ixinit <asrtemmashenkoff20158@gmail.com>
2022-12-04 15:03:48 +0300

    Added RezervDbManager with controller

---
+++ Games_WebApi/Controllers/RezervController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Games_WebApi.Rezerv;
+
+namespace Games_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RezervController : Controller
+    {
+        RezervDbManager rezerv;
+        public RezervController()
+        {
+            rezerv = new RezervDbManager();
+        }
+
+        [HttpGet]
+        public List<string> Index()
+        {
+            rezerv = new RezervDbManager();
+            Console.WriteLine("Get files path");
+            return rezerv.DataBases;
+        }
+
+        [HttpGet]
+        [Route("Create")]
+        public ActionResult Create()
+        {
+            rezerv.createCopy();
+            Console.WriteLine("Create db copy.");
+            return new OkResult();
+        }
+
+        [HttpGet]
+        [Route("Curent")]
+        public string Curent()
+        {
+            Console.WriteLine("Get curent db file");
+            return rezerv.CurentDbFile;
+        }
+
+        [HttpGet]
+        [Route("Curent/{id}")]
+        public ActionResult setDb(int id)
+        {
+            rezerv.setNewDbFile(id);
+            return new OkResult();
+        }
+
+        [HttpGet]
+        [Route("Delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            rezerv.deleteCopy(id);
+            Console.WriteLine($"Delete db copy {id}");
+            return new OkResult();
+        }
+
+    }
+}
---
+++ Games_WebApi/Rezerv/RezervDbManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Games_WebApi.Rezerv
+{
+
+    public class RezervDbManager
+    {
+        String curentDbFile;
+        String dbPath = "DataBase/";
+
+        List<String> databases;
+
+        public String CurentDbFile { get => curentDbFile; }
+        public List<String> DataBases { get => databases; }
+
+        public RezervDbManager()
+        {
+            createDbPath();
+            getCurentDbFile();
+            getDataBases();
+        }
+        public void createDbPath()
+        {
+            if (!Directory.Exists(dbPath))
+                Directory.CreateDirectory(dbPath);
+        }
+
+        public void getDataBases()
+        {
+            databases = new List<String>();
+            foreach (string filename in Directory.GetFiles(dbPath))
+            {
+                if(filename.EndsWith(".mdb"))
+                    databases.Add(filename);
+            }
+        }
+
+        public void getCurentDbFile()
+        {
+            curentDbFile = GameShopContext.connectionString.Substring(45); 
+        }
+
+        public void setNewDbFile(int index)
+        {
+            GameShopContext.connectionString = GameShopContext.connectionString.Substring(0, 45) + databases[index];
+            Console.WriteLine($"New Prowider: {GameShopContext.connectionString}");
+        }
+
+        public void deleteCopy(int id)
+        {
+            if (curentDbFile.Equals(databases[id]))
+            {
+                File.Delete(databases[id]);
+                getDataBases();
+                setNewDbFile(0);
+                getCurentDbFile();
+            }
+            else
+            {
+                File.Delete(databases[id]);
+                getDataBases();
+            }
+        }
+
+        public void createCopy()
+        {
+            File.Copy(curentDbFile, $"{dbPath}db_copy_{DateTime.Now.ToString().Replace(' ', '_').Replace(':','.')}.mdb");
+        }
+
+    }
+}
